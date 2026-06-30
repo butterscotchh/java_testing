17a7e570b2275daf02175c6a76c946c81711bb85
butterscotchh <sampansedang@gmail.com>
2026-06-30 10:10:11 +0700

    belajar tipe data

---
+++ src/NumberType.java
@@ -0,0 +1,19 @@
+public class NumberType {
+    static void main(String[] args) {
+
+        byte inibyte =  100;
+        short inishort = 1000;
+
+        int iniint = 1000000;
+        int decimalint = 10;
+        int hexadecimalint = 0xFFFF;
+        int binaryint = 0b10101010;
+
+        float inifloat = 10.90F;
+        double inidouble = 10.69;
+
+        long inilong = 1000000;
+        long inilong2 = 1000000L;
+        long inilong3 = 1_000_000L;
+    }
+}
--- src/psvm.java
+++ src/psvm.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
+public class psvm {
+    public static void main(String[] args) {
 
-public class jzxhckzcj {
+        System.out.println("hdsbjkds");
+
+    }
 }
