2a274c6d13c845e8310c7c64e34751a36c8bfee7
butterscotchh <sampansedang@gmail.com>
2026-06-30 10:26:10 +0700

    konversi number

---
+++ src/NumberConversion.java
@@ -0,0 +1,13 @@
+public class NumberConversion {
+    static void main(String[] args) {
+
+        byte inibyte = 20;
+        short inishort = inibyte;
+        int  iniint = inishort;
+
+        int iniint2 = 2000;
+        byte inibyte2 = (byte)iniint;
+
+        System.out.println(inishort);
+    }
+}
