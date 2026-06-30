04843db1cfcc786f2439bee53f4d641dbf3f66e9
butterscotchh <sampansedang@gmail.com>
2026-06-30 10:35:25 +0700

    belajar char

---
+++ src/CharacterType.java
@@ -0,0 +1,12 @@
+public class CharacterType {
+    static void main(String[] args) {
+
+        char a  = 'A';
+        char b = 'B';
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.print(a);
+        System.out.print(b);
+    }
+}
