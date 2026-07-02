3e503991095fabf7db55265524794b95f34cc6d1
butterscotchh <sampansedang@gmail.com>
2026-07-02 14:51:22 +0700

    switch yield

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -16,14 +16,32 @@ public class SwitchStatement {
                 break;
             default:
                 System.out.println("anda salah jurusan");
-        }
 
+        }
+        String ucapan;
         switch (nilai) {
-            case "A" -> System.out.println("wow anda lulus");
-            case "B", "C" -> System.out.println("Nilai anda lumayan");
-            case "D" -> System.out.println("anda tidak lulus");
-            default -> System.out.println("anda salah jurusan");
+            case "A" -> ucapan = ("wow anda lulus");
+            case "B", "C" -> ucapan = ("Nilai anda lumayan");
+            case "D" ->ucapan = ("anda tidak lulus");
+            default -> {
+                ucapan = ("anda salah jurusan");
+            }
         }
 
+        System.out.println(ucapan);
+
+        ucapan = switch (nilai) {
+            case "A":
+                yield "wow anda lulus";
+            case "B", "C":
+                yield "Nilai anda lumayan";
+            case "D":
+                yield "anda tidak lulus";
+            default : {
+                yield "anda salah jurusan";
+            }
+        };
+
+        System.out.println(ucapan);
     }
 }
