363595162ea9dba418e990008d192f2479ee5e0a
butterscotchh <sampansedang@gmail.com>
2026-07-02 13:29:36 +0700

    switch lambda

--- src/SwitchStatement.java
+++ src/SwitchStatement.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class SwitchStatement {
+
+    public static void main(String[] args) {
+
+        var nilai = "A";
+
+        switch (nilai) {
+            case "A":
+                System.out.println("wow anda lulus");
+                break;
+            case "B":
+            case "C":
+                System.out.println("nilai anda lumayan");
+            case "D":
+                System.out.println("anda tidak lulus");
+                break;
+            default:
+                System.out.println("anda salah jurusan");
+        }
+
+        switch (nilai) {
+            case "A" -> System.out.println("wow anda lulus");
+            case "B", "C" -> System.out.println("Nilai anda lumayan");
+            case "D" -> System.out.println("anda tidak lulus");
+            default -> System.out.println("anda salah jurusan");
+        }
+
+    }
 }
