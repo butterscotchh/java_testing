ea24258906bd3d7fae4a692135e74ebdc7987501
butterscotchh <sampansedang@gmail.com>
2026-07-02 15:33:12 +0700

    masih belajar ternary

---
+++ src/TernaryOperator.java
@@ -0,0 +1,14 @@
+public class TernaryOperator {
+    static void main(String[] args) {
+
+        var nilai = 75;
+        String ucapan;
+
+        if (nilai >= 75) {
+            ucapan = "anda lulus";
+        } else {
+            ucapan = "anda tidak lulus ";
+
+            System.out.println(ucapan);
+    }
+}
