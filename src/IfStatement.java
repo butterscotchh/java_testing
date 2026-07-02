e3766de0e1c987a4344ab6557d01cddd18473691
butterscotchh <sampansedang@gmail.com>
2026-07-02 11:14:51 +0700

    elseif

--- src/IfStatement.java
+++ src/IfStatement.java
@@ -1,13 +1,19 @@
 public class IfStatement {
     public static void main(String[] args) {
 
-        var nilai = 70;
+        var nilai = 75;
         var absen = 90;
 
-        if(nilai >= 75 && absen >= 75){
-            System.out.println("masuk UI");
-        } else {
-            System.out.println("masuk Unpam");
+        if(nilai >= 85 && absen >= 85){
+            System.out.println("Nilai A");
+        } else if(nilai >= 80 && absen >= 80) {
+            System.out.println("Nilai A-");
+        } else if(nilai >= 75 && absen >= 75) {
+            System.out.println("Nilai A-");
+        } else if(nilai >= 50 && absen >= 50) {
+            System.out.println("Nilai C");
+        } else if(nilai >= 0 && absen >= 0) {
+            System.out.println("Nilai F");
         }
 
     }
