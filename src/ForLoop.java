7e102af187f378106bfc7032e99226736ab6cd5b
butterscotchh <sampansedang@gmail.com>
2026-07-03 10:40:14 +0700

    looping java

---
+++ src/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        var counter = 9;
+
+        for(; counter < 10; counter++){
+            System.out.println("Nilai");
+        }
+    }
+}
