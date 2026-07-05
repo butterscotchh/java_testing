6fb37f87a2dda677809a6c0887397fc63735f21c
butterscotchh <sampansedang@gmail.com>
2026-07-06 06:09:35 +0700

    loop kelar

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,10 +1,10 @@
 public class ForLoop {
     public static void main(String[] args) {
 
-        var counter = 9;
 
-        for(; counter < 10; counter++){
-            System.out.println("Nilai");
+
+        for(var counter = 1; counter <= 10; counter++){
+            System.out.println("Nilai" + counter);
         }
     }
 }
