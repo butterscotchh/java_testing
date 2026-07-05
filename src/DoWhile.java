1be9c60124cae3ae9ebe2ac11c4ee34c32a0cb29
butterscotchh <sampansedang@gmail.com>
2026-07-06 06:17:34 +0700

    do while loop

---
+++ src/DoWhile.java
@@ -0,0 +1,11 @@
+public class DoWhile {
+    public static void main(String[] args) {
+
+        var counter = 100;
+
+
+        do {
+            System.out.println("Nilai" + counter++);
+        }while(counter <= 10);
+    }
+}
