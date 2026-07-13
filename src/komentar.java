629edea629956a1ac498a03bf633059e663b7141
butterscotchh <sampansedang@gmail.com>
2026-07-13 16:20:28 +0700

    komentar selesai next oop

---
+++ src/komentar.java
@@ -0,0 +1,19 @@
+public class komentar {
+    public static void sayhello(String sayhello) {
+
+        System.out.println(sum(10, 10));
+    }
+
+    /**
+     * penjumlahan a + b
+     * @param a
+     * @param b
+     * @return hasil
+     */
+    // @ + ctrl + space
+
+    static int sum(int a, int b) {
+        // pemjumlahan
+        return a + b;
+    }
+}
